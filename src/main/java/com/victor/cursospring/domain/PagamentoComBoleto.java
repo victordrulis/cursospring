614fae13d4e714afd79113108a832b41377b6f23
Victor Drulis <victordrulis@gmail.com>
2018-11-08 15:35:33 -0200

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/victor/cursospring/domain/PagamentoComBoleto.java
+++ src/main/java/com/victor/cursospring/domain/PagamentoComBoleto.java
@@ -5,13 +5,17 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.victor.cursospring.domain.enums.EstadoPagamento;
 
 @Entity
 public class PagamentoComBoleto extends Pagamento implements Serializable{
     private static final long serialVersionUID = 1L;
     
+    @JsonFormat(pattern="dd/MM/yyyy HH:mm")
     private Date dataVencimento;
+    
+    @JsonFormat(pattern="dd/MM/yyyy HH:mm")
     private Date dataPagamento;
     
     public PagamentoComBoleto () {}
