c3de373849343def15a1d04f7d6bf32a5fcacdda
Victor Drulis <victordrulis@gmail.com>
2018-11-09 14:37:16 -0200

    Validacao sintatica com Bean Validation

--- src/main/java/com/victor/cursospring/services/CategoriaService.java
+++ src/main/java/com/victor/cursospring/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.victor.cursospring.domain.Categoria;
+import com.victor.cursospring.dto.CategoriaDTO;
 import com.victor.cursospring.repositories.CategoriaRepository;
 import com.victor.cursospring.services.exceptions.DataIntegrityException;
 import com.victor.cursospring.services.exceptions.ObjectNotFoundException;
@@ -54,6 +55,10 @@ public class CategoriaService {
     public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
         PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
         return repo.findAll(pageRequest);
+    }
+    
+    public Categoria fromDTO(CategoriaDTO objDTO) {
+        return new Categoria(objDTO.getId(), objDTO.getNome());
     }
     
 }
