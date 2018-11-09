e6c65d874c26a9471040d1040dc194dc2d164abf
Victor Drulis <victordrulis@gmail.com>
2018-11-09 15:45:47 -0200

    Personalizando o retorno da validacao

---
+++ src/main/java/com/victor/cursospring/resources/exception/FieldMessage.java
@@ -0,0 +1,47 @@
+package com.victor.cursospring.resources.exception;
+
+import java.io.Serializable;
+
+public class FieldMessage implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    private String fieldName;
+    private String message;
+    
+    public FieldMessage() {}
+
+    public FieldMessage(String fieldName, String message) {
+        super();
+        this.fieldName = fieldName;
+        this.message = message;
+    }
+
+    /**
+     * @return the fieldName
+     */
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    /**
+     * @param fieldName the fieldName to set
+     */
+    public void setFieldName(String fieldName) {
+        this.fieldName = fieldName;
+    }
+
+    /**
+     * @return the message
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * @param message the message to set
+     */
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
