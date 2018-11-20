072f58ec3c8293ca39556c95287f40fb09423664
Victor Drulis <victordrulis@gmail.com>
2018-11-20 11:59:40 -0200

    Email HTML

--- src/main/java/com/victor/cursospring/services/PedidoService.java
+++ src/main/java/com/victor/cursospring/services/PedidoService.java
@@ -71,7 +71,7 @@ public class PedidoService {
         }
 
         itemPedidoRepo.saveAll(obj.getItens());
-        emailService.sendOrderConfirmationEmail(obj);
+        emailService.sendOrderConfirmationHtmlEmail(obj);
         return obj;
     }
 }
