3327fa3eef3167ed2b03be38c385c823b7082f28
Victor Drulis <victordrulis@gmail.com>
2018-11-22 23:40:15 -0200

    Refresh token

--- src/main/java/com/victor/cursospring/config/SecurityConfig.java
+++ src/main/java/com/victor/cursospring/config/SecurityConfig.java
@@ -38,9 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private JWTUtil jwtUtil;
     
     private static final String[] PUBLIC_MATCHERS = {
-            "/h2-console/**",
-            "/produtos/**",
-            "/categorias/**"
+            "/h2-console/**"
     };
     
     private static final String[] PUBLIC_MATCHERS_GET = {
